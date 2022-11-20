2be0ab181269f28c3e2c7dac2ceae4b0a9bf58fc
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2022-11-20 18:40:56 +0530

    revert the VFO step font size reduction

--- vfo.c
+++ vfo.c
@@ -1039,12 +1039,12 @@ void vfo_update() {
         cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
         cairo_show_text(cr, vfo_texts[0]);
         // show the step digit and the rest in grey
-        cairo_set_font_size(cr, 50);
-        cairo_set_source_rgb(cr, 0.75, 0.75, 0.75);
+        // cairo_set_font_size(cr, 50);
+        cairo_set_source_rgb(cr, 0.0, 1.0, 1.0); // 0.75, 0.75, 0.75);
         cairo_show_text(cr, ".");
-	cairo_set_source_rgb(cr, 0.75, 0.75, 0.75);
+	cairo_set_source_rgb(cr, 0.75, 1.0, 1.0); // 0.75, 0.75);
         cairo_show_text(cr, vfo_texts[1]);
-	cairo_set_source_rgb(cr, 0.75, 0.75, 0.75);
+	cairo_set_source_rgb(cr, 0.75, 1.0, 1.0); // 0.75, 0.75);
         cairo_show_text(cr, vfo_texts[2]);
 
         cairo_select_font_face(cr, "Cantarell",
