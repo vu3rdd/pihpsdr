vu3rdd/pihpsdr

commit 1ba053070465e2483ef08fba8229493bd59d945c

bugfix: allocate memory before copying the device name

rx_menu.c
@@ -202,6 +202,7 @@ void toggle_audio_output_device(void) {
     }
 
     char *new_output_device_name = output_devices[out_index].name;
+    active_receiver->audio_name = g_new(gchar, strlen(new_output_device_name) + 1);
     strcpy(active_receiver->audio_name, new_output_device_name);
 
     // set the other device as active

commit 5ade1f3c015465502e19df800a4d266d7efd4ed0

rx_menu: fix memory leak introduced in 1ba053070

rx_menu.c
@@ -201,6 +201,12 @@ void toggle_audio_output_device(void) {
 	audio_close_output(active_receiver);
     }
 
+    // deallocate memory for the name of the previous device.
+    if (active_receiver->audio_name != NULL) {
+        g_free(active_receiver->audio_name);
+        active_receiver->audio_name = NULL;
+    }
+
     char *new_output_device_name = output_devices[out_index].name;
     active_receiver->audio_name = g_new(gchar, strlen(new_output_device_name) + 1);
     strcpy(active_receiver->audio_name, new_output_device_name);